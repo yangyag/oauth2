1438061a007f99ce1fc01b3ed742c23ccce9d99c
yangyag <yangyag@hanmail.net>
2024-03-29 14:52:19 +0900

    Redis Publish 추가

--- src/main/java/com/yangyag/yangyagoauth/config/SecurityConfig.java
+++ src/main/java/com/yangyag/yangyagoauth/config/SecurityConfig.java
@@ -56,7 +56,6 @@ public class SecurityConfig {
                                 new AntPathRequestMatcher("/"),
                                 new AntPathRequestMatcher("/hello"),
                                 new AntPathRequestMatcher("/success"),
-                                new AntPathRequestMatcher("/publish"),
                                 new AntPathRequestMatcher("/api.html")
                                 ).permitAll()
                 .anyRequest().authenticated()
