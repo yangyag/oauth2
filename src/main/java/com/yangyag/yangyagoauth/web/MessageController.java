fefd688e59562a25e0e2c9752ef58e4e93d22f13
yangyag <yangyag@hanmail.net>
2024-04-03 13:57:18 +0900

    Kafka 멀티 브로커 구성

--- src/main/java/com/yangyag/yangyagoauth/web/MessageController.java
+++ src/main/java/com/yangyag/yangyagoauth/web/MessageController.java
@@ -14,7 +14,10 @@ public class MessageController {
     private final Map<String, MessagePublisher> messagePublisher;
 
     @PostMapping("/{messagingSystem}")
-    public String sendMessage(@PathVariable String messagingSystem, @RequestParam String topic, @RequestParam String message) {
+    public String sendMessage(@PathVariable String messagingSystem,
+                              @RequestParam String topic,
+                              @RequestParam String message) {
+
         messagePublisher.get(messagingSystem).sendMessage(topic, message);
 
         return "Message has been sent successful";
